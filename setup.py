e0ba5a10696f4fbfd9b4f6a432cc6ffaa7871161
Chris Hoy <chrisnhoy@gmail.com>
2025-10-06 23:36:34 +0100

    Add license

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="energy-controller",
     version="0.1.0",
+    description=(
+        "A web service for monitoring and controlling energy usage "
+        "with Octopus Energy"
+    ),
+    author="Chris Hoy",
+    license="MIT",
     packages=find_packages(),
     install_requires=[
         "flask",
@@ -10,4 +16,12 @@ setup(
         "pytz",
         "python-dotenv",
     ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.13",
+    ],
+    python_requires=">=3.8",
 )
