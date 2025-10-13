6f1310a8924361ba7bb35d865426559df4d24355
Chris Hoy <chrisnhoy@gmail.com>
2025-10-13 10:57:26 +0100

    Added Vue + Vite

---
+++ front-end/src/views/EnergyRates.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="energy-rates">
+    <h1>This is the energy rates page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .energy-rates {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
